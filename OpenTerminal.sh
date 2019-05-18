f244e3f6ae52fb8b6014f9521827015d7fb140b3
Uzun Vasili <vuzun@best4u.md>
2019-05-19 02:31:47 +0300

    Added drop trials, open terminal scripts, remove derived data script

---
+++ OpenTerminal.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+PROJ_NAME=${XcodeWorkspace%.*}
+osascript /Users/best4u/Scripts/OpenTerminal.scpt "$PROJ_NAME by XCode" "$PWD"
+
---
+++ RemoveDerivedData.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+rm -rf ~/Library/Developer/Xcode/DerivedData
+rm -frd ~/Library/Caches/com.apple.dt.Xcode/*
