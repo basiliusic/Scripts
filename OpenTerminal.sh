d9928d3d2f64760109740dd94ed720b16a713cd6
Uzun Vasili <vuzun@best4u.md>
2019-10-21 17:00:54 +0300

    Fix bash script

--- OpenTerminal.sh
+++ OpenTerminal.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Run Open terminal script
-
+SCRIPT_PATH=`dirname "$0"`
+echo $SCRIPT_PATH
 PROJ_NAME=${XcodeWorkspace%.*}
-osascript /Users/best4u/Scripts/OpenTerminal.scpt $PROJ_NAME "cd \"$PWD\" && clear"
+osascript $SCRIPT_PATH/OpenTerminal.scpt $PROJ_NAME "cd \"$PWD\" && clear"
 
