dabf6717b0847a06c378bdbb82bf6bd15d6c52cb
Uzun Vasili <vuzun@best4u.md>
2019-10-21 16:50:48 +0300

    Update script

--- OpenTerminal.sh
+++ OpenTerminal.sh
@@ -3,5 +3,5 @@
 # Run Open terminal script
 
 PROJ_NAME=${XcodeWorkspace%.*}
-osascript /Users/best4u/Scripts/OpenTerminal.scpt "$PROJ_NAME by XCode" "$PWD"
+osascript /Users/best4u/Scripts/OpenTerminal.scpt $PROJ_NAME "cd \"$PWD\" && clear"
 
