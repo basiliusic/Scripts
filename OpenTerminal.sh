c3b6df0ce1d80627d0eded633e85d5d16ac2db09
Uzun Vasili <vuzun@best4u.md>
2019-10-22 10:36:03 +0300

    Remove echo in bash script

--- OpenTerminal.sh
+++ OpenTerminal.sh
@@ -2,7 +2,6 @@
 
 # Run Open terminal script
 SCRIPT_PATH=`dirname "$0"`
-echo $SCRIPT_PATH
 PROJ_NAME=${XcodeWorkspace%.*}
 osascript $SCRIPT_PATH/OpenTerminal.scpt $PROJ_NAME "cd \"$PWD\" && clear"
 
